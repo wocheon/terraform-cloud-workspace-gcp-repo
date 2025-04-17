0af3d075ad64422a9b54d46f1a369296f59f7fc0
wocheon <wocheon07@gmail.com>
2025-04-17 09:37:55 +0900

    1st commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.29.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+module "gce_disk" {
+  source = "../modules/gce_disk"
+  new_disk_name         = var.new_disk_name
+  new_disk_zone         = var.new_disk_zone
+  new_disk_type         = var.new_disk_type
+  new_disk_size         = var.new_disk_size
+  new_disk_labels       = var.new_disk_labels
+### Source Configurations ###
+  new_disk_image_id              = var.new_disk_image_id
+  new_disk_snapshot_id           = var.new_disk_snapshot_id  
+}
+
+output "gce_disk_id" {
+  description = "Show GCE disk ID"
+  value       = module.gce_disk.disk_id
+}
+
+output "gce_disk_self_link" {
+  description = "Show GCE disk ID"
+  value       = module.gce_disk.self_link
+}
---
+++ terraform.tfvars
@@ -0,0 +1,22 @@
+### GCP Project&Region ###
+project       = "gcp-in-ca"
+region        = "asia-northeast3"
+zone          = "asia-northeast3-a"
+
+### Boot_disk Configurations ###
+new_disk_name = "gce-terraform-disk-test"
+new_disk_size = 30	#number
+new_disk_type = "pd-standard"
+new_disk_zone = "asia-northeast3-a"
+
+new_disk_labels = {
+   type = "gce-boot-disk"
+   user = "wocheon07"
+}
+
+### Source Configurations ###
+# 둘중 Null이 아닌 값을 찾아 사용
+new_disk_image_id       = null
+new_disk_snapshot_id    = null
+#new_disk_image_id		   = "5684785941229355772"
+#new_disk_snapshot_id    = "8732534544530120257"
